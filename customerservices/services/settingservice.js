5f19801d0d5d258cdb704648687aaa6c3d1758c9
sanju Malik <sanju.malik@schemaphic.com>
2024-12-31 18:53:21 +0530

    Customer vourchers

--- customerservices/services/settingservice.js
+++ customerservices/services/settingservice.js
@@ -15,9 +15,15 @@ async function getCustomerVoucherList(data) {
     if (!data.customer_id) {
       return { status: 400, message: "Missing required fields" };
     }
-    const vouchersSnapshot = await db.collection("customer_vouchers").where("customer_id", "==", data.customer_id).get();
+    const customervouchersSnapshot = await db.collection("customer_vouchers").where("customer_id", "==", data.customer_id).get();
+    const vouchersSnapshot = await db.collection("vouchers").get();
+
+    const customers = customervouchersSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
     const vouchers = vouchersSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    return { status: 200, message: "customer voucher list fatched successfully", vouchers };
+    const joinedData = customers.map(cust => { return { ...cust, voucher: vouchers.find(vou => vou.id === cust.voucher_id) }; });
+
+
+    return { status: 200, message: "customer voucher list fatched successfully", joinedData };
   } catch (error) {
     console.error("Error in getVoucherList:", error);
     return { status: 500, message: "Error fetching vouchers", error: error.message };
