df1563cc7ed5ac517dbf317151ea89b02eea20b3
sanju Malik <sanju.malik@schemaphic.com>
2025-02-01 18:39:36 +0530

    setting list

--- settingservices/services/homeservice.js
+++ settingservices/services/homeservice.js
@@ -72,8 +72,8 @@ async function getSettings(data) {
       query = query.where("user_type", "==", data.user_type);
 
     }
-    if(data.sortdata){
-      if (data.sortdata.search_text && data.sortdata.search_text!="") {      
+    if(data.sortdata){      
+      if (data.sortdata.sort_text && data.sortdata.sort_text!="") {     
         query = query.orderBy("user_type", data.sortdata.order);
       }else{
         query = query.orderBy("timestamp", "desc");
