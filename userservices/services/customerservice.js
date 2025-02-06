66862ba384e6bc5272656f04b57cb4f892f09867
sanju Malik <sanju.malik@schemaphic.com>
2025-02-06 19:17:03 +0530

    customer status

--- userservices/services/customerservice.js
+++ userservices/services/customerservice.js
@@ -47,7 +47,7 @@ async function registerCustomer(data) {
       passwordHash: hash,
       email: data.email,
       isVerified: false,
-      account_status:"Active",
+      status:"Active",
       createdAt: admin.firestore.FieldValue.serverTimestamp(),
     });
 
