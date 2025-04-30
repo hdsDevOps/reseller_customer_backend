9a6570a20ce3b9d3f33588f43be17523a5538601
HeshamReza <hesham.reza@schemaphic.com>
2025-04-30 17:35:12 +0530

    update on log generator

--- notificationservices/middleware/postLogger.js
+++ notificationservices/middleware/postLogger.js
@@ -56,7 +56,7 @@ function postLogger(req, res, next) {
       message: logMessage,
       at_time: new Date().toISOString(),
       method: req.method,
-      elapsed_time: elapsedTime,
+      elapsed_time: elapsedTime, 
     });
   });
 
